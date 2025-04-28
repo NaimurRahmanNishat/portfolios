bf3a1bc2c0fa379ed7a5f470d5efb478c97e653b
Naimur Rahman Nishat <naimurrahmun34@gmail.com>
2025-04-28 16:00:04 +0600

    added footer

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='pt-10 sm:pt-12 md:pt-14 lg:pt-16'>
+      <div className='px-4 sm:px-8 md:px-16 lg:px-32 xl:px-64 2xl:px-84 border-t-2 border-black'>
+        <p className='flex justify-center py-4 gap-1'>copyright &copy; {new Date().getFullYear()} <span className='text-lightGreen border-b border-lightGreen'>Naimur Rahman</span></p>
+      </div>
+    </footer>
   )
 }
 
-export default Footer;
+export default Footer; 
